82ed74dfeeac4607d450475bff69cd0c20c68d8a
Nabilbentamer <76843256+Nabilbentamer@users.noreply.github.com>
2021-06-09 15:51:56 +0100

    add necessary dependencies to app/gradle

--- app/src/main/java/com/example/casafoodie/DisplayRestaurants.java
+++ app/src/main/java/com/example/casafoodie/DisplayRestaurants.java
@@ -1,21 +1,42 @@
 package com.example.casafoodie;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
 
+import com.example.casafoodie.Adapter.RestaurantsListAdapter;
+import com.example.casafoodie.Model.Restaurants;
 import com.google.android.material.navigation.NavigationView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayRestaurants extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+
+
+    private RecyclerView recyclerView;
+    private List<Restaurants> RestauList;
+    private RestaurantsListAdapter myAdapter;
+    private DatabaseReference databaseReference;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -38,6 +59,46 @@ public class DisplayRestaurants extends AppCompatActivity {
         ActionBarDrawerToggle myToogle = new ActionBarDrawerToggle(this,drawerLayout, toolbar ,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(myToogle);
         myToogle.syncState();
+
+        // The adapater
+
+        recyclerView = findViewById(R.id.TeacherListRV);
+
+        TeacherList = new ArrayList<>();
+        myAdapter = new TeacherListAdapter(this,TeacherList);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Teachers");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        databaseReference.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot!=null){
+                    for( DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                        Teacher teacher = dataSnapshot1.getValue(Teacher.class);
+                        TeacherList.add(teacher);
+
+                    }
+
+                }
+
+
+
+                myAdapter.notifyDataSetChanged();
+                myAdapter = new TeacherListAdapter(professeurList.this,TeacherList);
+                recyclerView.setAdapter(myAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: ");
+
+
+            }
+        });
 
 
 
