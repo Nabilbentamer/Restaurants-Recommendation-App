71e6031a43efba4d134d6b0299f19acabcd5e881
Nabilbentamer <76843256+Nabilbentamer@users.noreply.github.com>
2021-06-09 13:35:21 +0100

    add Favorite_restaurants model

--- app/src/main/java/com/example/casafoodie/Model/Favorite_restaurants.java
+++ app/src/main/java/com/example/casafoodie/Model/Favorite_restaurants.java
@@ -2,4 +2,29 @@ package com.example.casafoodie.Model;
 
 public class Favorite_restaurants {
 
+    String restau_id,user_phone;
+
+    public Favorite_restaurants(String restau_id, String user_phone) {
+        this.restau_id = restau_id;
+        this.user_phone = user_phone;
+    }
+
+    public Favorite_restaurants() {
+    }
+
+    public void setRestau_id(String restau_id) {
+        this.restau_id = restau_id;
+    }
+
+    public void setUser_phone(String user_phone) {
+        this.user_phone = user_phone;
+    }
+
+    public String getRestau_id() {
+        return restau_id;
+    }
+
+    public String getUser_phone() {
+        return user_phone;
+    }
 }
