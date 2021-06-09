74c4a4babcf9eaaade8791733346b52615b8a628
Nabilbentamer <76843256+Nabilbentamer@users.noreply.github.com>
2021-06-09 15:34:55 +0100

    add restaurant_item.xml & RestaurantsListAdapter

---
+++ app/src/main/java/com/example/casafoodie/Adapter/RestaurantsListAdapter.java
@@ -0,0 +1,83 @@
+package com.example.casafoodie.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import java.util.List;
+import com.example.casafoodie.Model.Restaurants;
+import com.example.casafoodie.R;
+
+import java.util.List;
+
+public class RestaurantsListAdapter extends RecyclerView.Adapter<RestaurantsListAdapter.RestaurantsListViewHolder> {
+
+    private Context context;
+    private List<Restaurants> RestauList;
+
+    public RestaurantsListAdapter(Context context, List<Restaurants> TeacherList) {
+        this.context = context;
+        this.RestauList = TeacherList;
+
+    }
+
+    public RestaurantsListAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public RestaurantsListAdapter.RestaurantsListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View ItemView = layoutInflater.inflate(R.layout.restaurant_item,parent,false);
+        RestaurantsListViewHolder restaurantsListViewHolder = new RestaurantsListViewHolder(ItemView);
+
+        return restaurantsListViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RestaurantsListAdapter.RestaurantsListViewHolder holder, int position) {
+
+        Restaurants restaurants = RestauList.get(position);
+
+
+        holder.professor_item_name_tv.setText(restaurants.getName());
+        holder.professor_item_email_tv.setText("Email: " + restaurants.getPrice() + "\n" +
+                "Matière ensignées: " + restaurants.getGood_for() + "\n"
+        );
+        //"Address: " + teacher.getAddress()
+
+        Glide.with(context).load(restaurants.getImage()).into(holder.professor_item_name_iv);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return RestauList.size();
+    }
+
+    public class RestaurantsListViewHolder extends RecyclerView.ViewHolder {
+        TextView professor_item_name_tv,professor_item_email_tv;
+        ImageView professor_item_name_iv;
+
+        public RestaurantsListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            professor_item_name_tv= itemView.findViewById(R.id.professor_item_name_tv);
+            professor_item_email_tv= itemView.findViewById(R.id.professor_item_email_tv);
+            professor_item_name_iv= itemView.findViewById(R.id.professor_item_name_iv);
+        }
+    }
+}
